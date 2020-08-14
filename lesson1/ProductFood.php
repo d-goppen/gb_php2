87f625f878aee9c864f04df7547f5fee0d288175
d-goppen <51300326+d-goppen@users.noreply.github.com>
2020-08-14 12:21:06 +0300

    lesson 1 done

---
+++ lesson1/ProductFood.php
@@ -0,0 +1,50 @@
+<?php
+
+class ProductFood extends Product
+{
+    // дата производства
+    protected $manufactured;
+    // срок годности (месяцев)
+    protected $shelfLife;
+
+    /**
+     * @return int
+     */
+    public function getManufactured(): int
+    {
+        return $this->manufactured;
+    }
+
+    /**
+     * @param int $newDate
+     */
+    public function setManufactured(int $newDate) {
+        $this->manufactured = $newDate;
+    }
+
+    /**
+     * @return int
+     */
+    public function getShellLife(): int
+    {
+        return $this->shellLife;
+    }
+
+    /**
+     * @param int $newShellLife
+     */
+    public function setShellLife(int $newShellLife) {
+        $this->shellLife = $newShellLife;
+    }
+
+    /**
+     * @return int
+     */
+    // Пример вычисляемого read-only свойства
+    public function getExpired(): int
+    {
+        // Черновое вычисление даты окончания срока годности
+        // без учёта количества дней в месяце и т.п.
+        return $this->manufactured + $this->shellLife * 30 * 24 * 3600 * 1000;
+    }
+}
